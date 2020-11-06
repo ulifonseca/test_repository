9cbc4e6f29e54ef7c8dc77b383309cf43cb34c19
Ulises <ulisesfonseca35@gmail.com>
2020-11-05 20:53:59 -0600

    version 4

--- hello.c
+++ hello.c
@@ -4,5 +4,10 @@ int main(void)
 {
     printf("Dalila\n");
     printf("Fonseca Maya\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ",i+2);
+    }
+    printf("\n");
     return 0;
 }
